a5e7ca64ca6058ec09843a0e3c07952a522636c3
Fabio Valim <oficialvalim@gmail.com>
2024-09-10 15:33:51 -0300

    listar/cadastrar metas

--- index.js
+++ index.js
@@ -1,33 +1,67 @@
-const { select } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
+
+let meta = { value: "Tomar 3L de água", checked: false }
+
+let metas = [meta]
+
+async function cadastrarMeta() {
+    const meta = await input({ message: "\nDigite a meta:" })
+
+    if(meta.length == 0) {
+        console.log("\nA meta não pode ser vazia.\n")
+        return
+    }
+
+    metas.push({ value: meta, checked: false })
+}
+
+async function listarMetas() {
+    const respostas = await checkbox({
+        message: "\n< Use as SETAS para mudar de meta, o ESPAÇO para marcar/desmarcar, e o ENTER para enviar >",
+        choices: [...metas], 
+        instructions: false
+    })
+
+    if(respostas.length == 0) {
+        console.los("\nNenhuma meta selecionada :(\n")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log("\nMarcação realizada com sucesso!\n")
+}
 
 async function start() {
-    
+
     while(true) {
         
-        const opcao  = await select({
+        const opcao  = await select({//Menu da aplicacao
             message: "Menu >",
             choices: [
-                {
-                    name: "Cadastrar meta",
-                    value: "cadastrar"
-                },
-                {
-                    name: "Listar metas",
-                    value: "listar"
-                },
-                {
-                    name: "Sair",
-                    value: "sair"
-                }
+                {name: "Cadastrar meta", value: "cadastrar"},
+                {name: "Listar metas", value: "listar"},
+                {name: "Sair", value: "sair"}
             ]
         })
 
         switch(opcao) {
             case "cadastrar":
-                console.log("\nVamos cadastrar\n")
+                await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
-                console.log("\nVamos listar\n")
+                await listarMetas()
                 break
             case "sair":
                 console.log("\nAté a próxima!\n")
--- toDo.md
+++ toDo.md
@@ -1,6 +1,6 @@
-- criar metas 
-- listar metas
-    - metas realizadas
+- criar metas [x]
+- listar metas 
+    - metas realizadas 
     - metas abertas
 - marcar / desmarcar metas realizadas
 - remover metas
